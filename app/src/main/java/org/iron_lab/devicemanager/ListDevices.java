61e817664705cddf3207056dd0635fc0ebd3145c
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-02-26 09:53:42 -0700

    sans-gitignore first commit

---
+++ app/src/main/java/org/iron_lab/devicemanager/ListDevices.java
@@ -0,0 +1,13 @@
+package org.iron_lab.devicemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_devices);
+    }
+}
