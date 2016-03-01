a7b03a528ec482a40d581c46134f6fdd6548e3f8
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-03-01 14:58:41 -0700

    added basic menu and structure for seperating lab goals into different Activities

--- app/src/main/java/org/iron_lab/devicemanager/AndroidMedia.java
+++ app/src/main/java/org/iron_lab/devicemanager/AndroidMedia.java
@@ -2,12 +2,16 @@ package org.iron_lab.devicemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class AndroidMedia extends AppCompatActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_media);
     }
+
 }
